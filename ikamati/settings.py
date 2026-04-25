d22a933acfb206429e135e0c838f636286ca7bd5
hani <hanimca16!@gmail.com>
2026-04-25 18:47:39 +0100

    fix allowed hosts

--- ikamati/settings.py
+++ ikamati/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w91z92&sd%!wotxnq(-*wi2aq#y2stm#is_#$lh53sxa*)(eiy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
