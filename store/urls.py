13b7eb9ea35b4bf593345bcbab9fbe4770a3c1ac
hani <hanimca16!@gmail.com>
2026-04-30 19:18:10 +0100

    fix google callback url

--- store/urls.py
+++ store/urls.py
@@ -1,17 +1,12 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('store/<str:residence>/', views.product_list, name='product_list'),
-    path('product/<int:pk>/', views.product_detail, name='product_detail'),
-    path('add-product/', views.add_product, name='add_product'),
-    path('delete-product/<int:pk>/', views.delete_product, name='delete_product'),
-    path('register/', views.register, name='register'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),
-    path('inbox/', views.inbox, name='inbox'),
-path('profile/', views.profile, name='profile'),
-path('import/', views.import_order, name='import_order'),
-path('import/orders/', views.my_orders, name='my_orders'),
-path('import/rate/<int:pk>/', views.rate_order, name='rate_order'),]
+    path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+    path('blog/', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('allauth.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
